17714c01d906a04e7972174fa724697bec0ba157
Poonam Chauhan <poonam@localhost.localdomain>
2018-03-01 14:19:55 +0530

    Everything about components in Vue

---
+++ vuejs-component/src/components/UserDetail.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+        <button @click="resetName()">Reset Name</button>
+        <button @click="resetFn">Reset Name</button>
+        <p>{{userAge}}</p>
+    </div>
+</template>
+
+<script>
+import {eventBus} from '../main';
+  export default {
+    /*props : {
+      {                 // Props validation
+      name : String,
+      required : true
+     },
+     restFn : Function,
+     userAge : Number
+    },*/
+    props : ['myName','resetFn','userAge'],
+    methods: {
+      switchName() {
+        return this.myName.split("").reverse().join("")
+      },
+      resetName(){
+        this.myName = "Poonam",
+        this.$emit('nameWasReset',this.myName)
+      }
+    },
+    created() {
+      eventBus.$on('ageWasChanged', (age) => this.userAge = age)
+    }
+  }
+
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
---
+++ vuejs-component/src/components/UserEdit.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="component">
+        <h3>You may edit the User here</h3>
+        <p>Edit me!</p>
+        <button @click="editAge">Edit age</button>
+        <p>{{userAge}}</p>
+    </div>
+</template>
+
+<script>
+import {eventBus} from '../main';
+
+export default {
+  props : ['userAge'],
+  methods: {
+    editAge() {
+      this.userAge = 30;
+      //this.$emit('ageWasChanged',this.userAge)
+      //eventBus.$emit('ageWasChanged',this.userAge);
+      eventBus.changeAge(this.userAge);
+    }
+  }
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightgreen;
+    }
+</style>
