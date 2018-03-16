f60ff937a1ba94b93792a04ba069284a3252c2fc
Poonam Chauhan <poonam@localhost.localdomain>
2018-03-16 20:13:32 +0530

    Animations used in quiz app

--- vuejs-animations/src/App.vue
+++ vuejs-animations/src/App.vue
@@ -50,6 +50,14 @@
                 <transition name="fade" mode="out-in">
                   <component :is="selectedComponent"></component>
                 </transition>
+                <hr>
+                <button class="btn btn-primary" @click="addItem">Add / Remove Item</button>
+                <br><br>
+                <ul class="list-group">
+                  <transition-group name="slide">
+                  <li class="list-group-item" v-for="(number,index) in numbers" @click="removeItem(index)" style="cursor:pointer" :key="number">{{number}}</li>
+                </transition-group>
+                </ul>
             </div>
         </div>
     </div>
@@ -65,7 +73,8 @@ import DangerAlert from './DangerAlert.vue';
               load : false,
               alertAnimation : 'fade',
               elementWidth : 100,
-              selectedComponent : 'app-success-alert'
+              selectedComponent : 'app-success-alert',
+              numbers : [1,2,3,4,5]
             }
         },
         methods: {
@@ -115,6 +124,13 @@ import DangerAlert from './DangerAlert.vue';
           },
           leaveCancelled(el){
             console.log("leaveCancelled");
+          },
+          addItem(){
+            const pos = Math.floor(Math.random() * this.numbers.length)
+            this.numbers.splice(pos,0,this.numbers.length+1)
+          },
+          removeItem(index){
+            this.numbers.splice(index,1)
           }
         },
         components:{
@@ -159,6 +175,11 @@ import DangerAlert from './DangerAlert.vue';
     animation: slide-out 1s ease-out forwards;
     transition: opacity 3s;
     opacity: 0;
+    position: absolute;
+  }
+
+  .slide-move{
+    transition: transform 1s;
   }
 
   @keyframes slide-in{
